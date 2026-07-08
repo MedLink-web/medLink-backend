8117a3a1cfb2fb80ab89f7fe0a6e365f72e751e7
Rawan Qaoud <rawanqaoud4@gmail.com>
2026-07-09 00:32:04 +0300

    bug fix

--- config/cors.php
+++ config/cors.php
@@ -5,7 +5,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'https://med-link-frontend-silk.vercel.app',
+    ],
 
     'allowed_origins_patterns' => [],
 
